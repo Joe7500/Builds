f40bf00b83fc41abe576b88fea84958c1f9a0ccb
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-02-18 22:06:57 +0200

    Update lineage-22.1.sh

--- crave/lineage-22.1.sh
+++ crave/lineage-22.1.sh
@@ -115,8 +115,9 @@ pip install --upgrade b2 ; check_fail
 b2 account authorize "$BKEY_ID" "$BAPP_KEY" > /dev/null 2>&1 ; check_fail
 mkdir priv-keys
 b2 sync "b2://$BUCKET_NAME/inline" "priv-keys" > /dev/null 2>&1 ; check_fail
-mkdir --parents vendor/lineage-priv/
-mv priv-keys vendor/lineage-priv/keys
+mkdir --parents vendor/lineage-priv/keys
+mv priv-keys/* vendor/lineage-priv/keys
+rm -rf priv-keys
 deactivate
 unset BUCKET_NAME
 unset KEY_ENCRYPTION_PASSWORD
