db7c7e201ef7a5a68d15808e85f68b5669bdb363
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-03-04 02:21:18 +0200

    Update crdroid-14.sh

--- crave/crdroid-14.sh
+++ crave/crdroid-14.sh
@@ -14,6 +14,7 @@ DEVICE_BRANCH=lineage-21
 VENDOR_BRANCH=lineage-21
 XIAOMI_BRANCH=lineage-21
 REPO_URL="-u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs"
+OTA_SED_STRING="crdroidandroid/android_vendor_crDroidOTA/14.0/{device}.json"
 export BUILD_USERNAME=user
 export BUILD_HOSTNAME=localhost 
 export KBUILD_BUILD_USER=user
@@ -99,7 +100,7 @@ rm -f packages/modules/Connectivity/staticlibs/device/com/android/net/module/uti
 
 cd packages/apps/Updater/ && git reset --hard && cd ../../../
 cp packages/apps/Updater/app/src/main/res/values/strings.xml strings.xml
-cat strings.xml | sed -e "s#crdroidandroid/android_vendor_crDroidOTA/14.0/{device}.json#Joe7500/Builds/main/$PACKAGE_NAME.$VARIANT_NAME.chime.json#g" > strings.xml.1
+cat strings.xml | sed -e "s#$OTA_SED_STRING#Joe7500/Builds/main/$PACKAGE_NAME.$VARIANT_NAME.chime.json#g" > strings.xml.1
 cp strings.xml.1 packages/apps/Updater/app/src/main/res/values/strings.xml
 check_fail
 
