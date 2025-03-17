59b4a720b8a0ead8b616f2bfa3b8a6fe441d33f5
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-03-17 23:32:09 +0200

    Update axion.sh

--- crave/axion.sh
+++ crave/axion.sh
@@ -10,7 +10,7 @@ set -v
 
 PACKAGE_NAME=axion
 VARIANT_NAME=user
-DEVICE_BRANCH=lineage-22.2
+DEVICE_BRANCH=lineage-22.2-test
 VENDOR_BRANCH=lineage-22
 XIAOMI_BRANCH=lineage-22.2
 REPO_URL="-u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs"
