910f225a75e40294a111fca4bcfbcb9c8fb2ca75
Joe7500 <62238620+Joe7500@users.noreply.github.com>
2025-03-26 23:38:57 +0200

    Update lineage-22-test.sh

--- crave/lineage-22-test.sh
+++ crave/lineage-22-test.sh
@@ -93,7 +93,7 @@ git clone https://github.com/Joe7500/device_xiaomi_chime.git -b $DEVICE_BRANCH d
 git clone https://github.com/Joe7500/vendor_xiaomi_chime.git -b $VENDOR_BRANCH vendor/xiaomi/chime ; check_fail
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b $XIAOMI_BRANCH hardware/xiaomi ; check_fail
 
-cd device/xiaomi/chime && git reset --hard && git reset --hard f34eba879da7d260a40d0da55f9a0ef52170b119 && check_fail
+cd device/xiaomi/chime && git reset --hard && git reset --hard f34eba879da7d260a40d0da55f9a0ef52170b119 ; check_fail
 
 patch -f -p 1 < wfdservice.rc.patch ; check_fail
 cd packages/modules/Connectivity/ && git reset --hard && cd ../../../
